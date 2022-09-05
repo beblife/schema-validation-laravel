0a3f8a23eb6d9185a43a5f2393df039a55bb9bdc
Laurens Bultynck <laurens@3daimtrainer.com>
2022-09-05 18:32:49 +0200

    Add IDE helper file

---
+++ _ide_helpers.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Illuminate\Http {
+    /**
+     * @method \Illuminate\Http\Request validateSchema(?\Beblife\SchemaValidation\Schema $schema = null)
+     */
+    class Request
+    {
+    }
+}
